8065b7cb24abef2f49fa818fd4284a99fa2a7091
Anisha KC <anisha.kc7219@gmail.com>
2020-07-07 22:50:28 -0500

    render a list of mock data using recyclerview

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.simpletodo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<String> items;
+
+    Button btnAdd;
+    EditText etItem;
+    RecyclerView rvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        etItem = findViewById(R.id.etItem);
+        rvItems = findViewById(R.id.rvItems);
+
+        // Mock data
+        items = new ArrayList<>();
+        items.add("Buy Milk");
+        items.add("Complete Assignment");
+        items.add("Buy gift");
+        items.add("Read book");
+        items.add("Finish Flutter Project");
+
+        ItemsAdaptor itemsAdaptor = new ItemsAdaptor(items);
+        rvItems.setAdapter(itemsAdaptor);
+        rvItems.setLayoutManager(new LinearLayoutManager(this));
+
+        // Render Mock Data in RecyclerView
+
     }
 }
